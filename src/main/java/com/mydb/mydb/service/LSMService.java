4dad678cab4f7ebd5f3720a8a98a49860b60a39b
niranjani <niranjani@sahajsoft.com>
2022-03-02 20:53:53 +0530

    Changing fixed delay for merging process

--- src/main/java/com/mydb/mydb/service/LSMService.java
+++ src/main/java/com/mydb/mydb/service/LSMService.java
@@ -46,7 +46,7 @@ public class LSMService {
     this.memTable = memTable;
   }
 
-  @Scheduled(initialDelay = 10000, fixedDelay = 15000)
+  @Scheduled(initialDelay = 10000, fixedDelay = 60000)
   public void merge() throws IOException {
 //    log.info("**************\nStarting scheduled merging!\n******************");
     final var segmentEnumeration = getSegmentIndexEnumeration();
